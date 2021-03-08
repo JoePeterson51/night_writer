3a23b28d291fd49d7d903bc3aee3af7ff2750586
JoePeterson51 <>
2021-03-07 23:44:40 -0700

    Translate multiple letters but incorrectly

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -12,12 +12,18 @@ class NightWriterTest < Minitest::Test
   def test_it_can_read_message
     night_writer = NightWriter.new
 
-    assert_equal "Created 'braille.txt' containing 256 characters", night_writer.read_file("test_message.txt")
+    assert_equal "h", night_writer.read_file("test_message.txt")
   end
 
   def test_dictionary
     night_writer = NightWriter.new
 
     assert_equal "0.\n..\n..", night_writer.dictionary["a"]
+  end
+
+  def test_it_can_translate_1_letter
+    night_writer = NightWriter.new
+
+    assert_equal ["0.\n00\n.."], night_writer.translate
   end
 end
